98df1c492d7a879673c1362fc615027dcc30a962
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-09-15 09:13:12 +0200

    Improve model utils

--- ray_elasticsearch/model.py
+++ ray_elasticsearch/model.py
@@ -1,25 +1,34 @@
 from typing import (
-    TYPE_CHECKING,
     Any,
     Literal,
     Mapping,
     Union,
+    TYPE_CHECKING,
 )
 from typing_extensions import TypeAlias  # type: ignore
 
-
-from ray_elasticsearch.elasticsearch_compat import ELASTICSEARCH_DSL_AVAILABLE
+from ray_elasticsearch._compat import BaseDocument, Document, Query
 
 
 # Determine index and query typing based on Elasticsearch DSL availability.
-if TYPE_CHECKING or ELASTICSEARCH_DSL_AVAILABLE:
-    from ray_elasticsearch.elasticsearch_compat import Document, Query
+if (
+    BaseDocument is not NotImplemented
+    and Document is not NotImplemented
+    and Query is not NotImplemented
+) or TYPE_CHECKING:
+    from ray_elasticsearch._compat import BaseDocument, Document, Query
 
-    IndexType: TypeAlias = Union[type[Document], str]
-    QueryType: TypeAlias = Union[Query, Mapping[str, Any]]
+    IndexType: TypeAlias = Union[type[BaseDocument], type[Document], str]  # type: ignore[no-redef]
+    QueryType: TypeAlias = Union[Query, Mapping[str, Any]]  # type: ignore[no-redef]
+elif BaseDocument is not NotImplemented:
+    IndexType: TypeAlias = Union[type[BaseDocument], str]  # type: ignore[no-redef,misc]
+    QueryType: TypeAlias = Mapping[str, Any]  # type: ignore[no-redef,misc]
+elif Document is not NotImplemented and Query is not NotImplemented:
+    IndexType: TypeAlias = Union[type[Document], str]  # type: ignore[no-redef,misc]
+    QueryType: TypeAlias = Union[Query, Mapping[str, Any]]  # type: ignore[no-redef,misc]
 else:
-    IndexType: TypeAlias = str  # type: ignore
-    QueryType: TypeAlias = Mapping[str, Any]  # type: ignore
+    IndexType: TypeAlias = str  # type: ignore[no-redef,misc]
+    QueryType: TypeAlias = Mapping[str, Any]  # type: ignore[no-redef,misc]
 
 
 OpType: TypeAlias = Literal["index", "create", "update", "delete"]
