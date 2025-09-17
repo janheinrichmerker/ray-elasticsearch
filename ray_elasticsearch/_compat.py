92386456ff341a618be4e3d3328ea32c284fd9a6
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-09-17 06:20:51 +0200

    Simplify compat imports

--- ray_elasticsearch/_compat.py
+++ ray_elasticsearch/_compat.py
@@ -1,111 +1,76 @@
-from typing import Optional, TYPE_CHECKING
+from importlib.metadata import metadata, PackageNotFoundError, version
+from packaging.version import Version
 
 
-# Elasticsearch imports (will use major-version-locked package or default)
-_import_error: Optional[ImportError] = ImportError()
-if _import_error is not None:
+def _is_installed(distribution_name: str) -> bool:
     try:
-        from elasticsearch8 import Elasticsearch as Elasticsearch  # type: ignore[no-redef]
-        from elasticsearch8.helpers import streaming_bulk as streaming_bulk  # type: ignore[no-redef]
-
-        _import_error = None
-    except ImportError as e:
-        _import_error = e
-if _import_error is not None:
-    try:
-        from elasticsearch7 import Elasticsearch as Elasticsearch  # type: ignore[no-redef,assignment]
-        from elasticsearch7.helpers import streaming_bulk as streaming_bulk  # type: ignore[no-redef,assignment]
-
-        _import_error = None
-    except ImportError as e:
-        _import_error = e
-if _import_error is not None:
-    try:
-        from elasticsearch6 import Elasticsearch as Elasticsearch  # type: ignore[no-redef,assignment]
-        from elasticsearch6.helpers import streaming_bulk as streaming_bulk  # type: ignore[no-redef,assignment]
-
-        _import_error = None
-    except ImportError as e:
-        _import_error = e
-if _import_error is not None:
-    try:
-        from elasticsearch import Elasticsearch as Elasticsearch  # type: ignore[no-redef,assignment]
-        from elasticsearch.helpers import streaming_bulk as streaming_bulk  # type: ignore[no-redef,assignment]
-
-        _import_error = None
-    except ImportError as e:
-        _import_error = e
-if _import_error is not None:
-    raise _import_error
+        metadata(distribution_name=distribution_name)
+        return True
+    except PackageNotFoundError:
+        return False
 
 
-# Elasticsearch DSL imports (will use major-version-locked package or default):
-_import_error_dsl: Optional[ImportError] = ImportError()
-if _import_error_dsl is not None:
-    try:
-        from elasticsearch8_dsl import (  # type: ignore[no-redef]
-            Document as Document,
-            InnerDoc as InnerDoc,
-            Field as Field,
-            Object as Object,
-            Nested as Nested,
-            Text as Text,
-        )
-        from elasticsearch8_dsl.query import Query as Query  # type: ignore[no-redef]
+def _is_version_at_least(distribution_name: str, at_least_version: str) -> bool:
+    version_str = version(distribution_name=distribution_name)
+    return Version(version_str) >= Version(at_least_version)
 
-        _import_error_dsl = None
-    except ImportError as e:
-        _import_error_dsl = e
-if _import_error_dsl is not None:
-    try:
-        from elasticsearch7_dsl import (  # type: ignore[no-redef,assignment]
-            Document as Document,
-            InnerDoc as InnerDoc,
-            Field as Field,
-            Object as Object,
-            Nested as Nested,
-            Text as Text,
-        )
-        from elasticsearch7_dsl.query import Query as Query  # type: ignore[no-redef,assignment]
 
-        _import_error_dsl = None
-    except ImportError as e:
-        _import_error_dsl = e
-if _import_error_dsl is not None:
-    try:
-        from elasticsearch6_dsl import (  # type: ignore[no-redef,assignment]
-            Document as Document,
-            InnerDoc as InnerDoc,
-            Field as Field,
-            Object as Object,
-            Nested as Nested,
-            Text as Text,
-        )
-        from elasticsearch6_dsl.query import Query as Query  # type: ignore[no-redef,assignment]
+if _is_installed("elasticsearch8"):
+    from elasticsearch8 import Elasticsearch as Elasticsearch  # type: ignore[no-redef]
+    from elasticsearch8.helpers import streaming_bulk as streaming_bulk  # type: ignore[no-redef]
+elif _is_installed("elasticsearch7"):
+    from elasticsearch7 import Elasticsearch as Elasticsearch  # type: ignore[no-redef,assignment]
+    from elasticsearch7.helpers import streaming_bulk as streaming_bulk  # type: ignore[no-redef,assignment]
+elif _is_installed("elasticsearch6"):
+    from elasticsearch6 import Elasticsearch as Elasticsearch  # type: ignore[no-redef,assignment]
+    from elasticsearch6.helpers import streaming_bulk as streaming_bulk  # type: ignore[no-redef,assignment]
+elif _is_installed("elasticsearch"):
+    from elasticsearch import Elasticsearch as Elasticsearch  # type: ignore[no-redef,assignment]
+    from elasticsearch.helpers import streaming_bulk as streaming_bulk  # type: ignore[no-redef,assignment]
+else:
+    raise ImportError("Elasticsearch is not installed.")
 
-        _import_error_dsl = None
-    except ImportError as e:
-        _import_error_dsl = e
-if _import_error_dsl is not None:
-    try:
-        from elasticsearch_dsl import (  # type: ignore[no-redef,assignment]
-            Document as Document,
-            InnerDoc as InnerDoc,
-            Field as Field,
-            Object as Object,
-            Nested as Nested,
-            Text as Text,
-        )
-        from elasticsearch_dsl.query import Query as Query  # type: ignore[no-redef,assignment]
-
-        _import_error_dsl = None
-    except ImportError as e:
-        _import_error_dsl = e
-if _import_error_dsl is not None and not TYPE_CHECKING:
-    Document = NotImplemented  # type: ignore
-    InnerDoc = NotImplemented  # type: ignore
-    Query = NotImplemented  # type: ignore
-    Field = NotImplemented  # type: ignore
-    Object = NotImplemented  # type: ignore
-    Nested = NotImplemented  # type: ignore
-    Text = NotImplemented  # type: ignore
+if _is_installed("elasticsearch8-dsl"):
+    if _is_version_at_least("elasticsearch8-dsl", "8.12.0"):
+        raise ImportError("Elasticsearch DSL version below 8.12.0 is required.")
+    from elasticsearch8_dsl import (  # type: ignore[no-redef]
+        Document as Document,
+        InnerDoc as InnerDoc,
+        Field as Field,
+        Object as Object,
+        Nested as Nested,
+        Text as Text,
+    )
+    from elasticsearch8_dsl.query import Query as Query  # type: ignore[no-redef]
+elif _is_installed("elasticsearch7-dsl"):
+    from elasticsearch7_dsl import (  # type: ignore[no-redef,assignment]
+        Document as Document,
+        InnerDoc as InnerDoc,
+        Field as Field,
+        Object as Object,
+        Nested as Nested,
+        Text as Text,
+    )
+    from elasticsearch7_dsl.query import Query as Query  # type: ignore[no-redef,assignment]
+elif _is_installed("elasticsearch6-dsl"):
+    from elasticsearch6_dsl import (  # type: ignore[no-redef,assignment]
+        Document as Document,
+        InnerDoc as InnerDoc,
+        Field as Field,
+        Object as Object,
+        Nested as Nested,
+        Text as Text,
+    )
+    from elasticsearch6_dsl.query import Query as Query  # type: ignore[no-redef,assignment]
+elif _is_installed("elasticsearch-dsl"):
+    from elasticsearch_dsl import (  # type: ignore[no-redef,assignment]
+        Document as Document,
+        InnerDoc as InnerDoc,
+        Field as Field,
+        Object as Object,
+        Nested as Nested,
+        Text as Text,
+    )
+    from elasticsearch_dsl.query import Query as Query  # type: ignore[no-redef,assignment]
+else:
+    raise ImportError("Elasticsearch DSL is not installed.")
